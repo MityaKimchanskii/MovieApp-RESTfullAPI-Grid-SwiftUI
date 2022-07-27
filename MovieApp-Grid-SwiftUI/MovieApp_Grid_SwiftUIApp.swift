1cc0bc80643b832ce792778aa0cc25d894c14aec
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-26 22:22:19 -0700

    Initial Commit

---
+++ MovieApp-Grid-SwiftUI/MovieApp_Grid_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp_Grid_SwiftUIApp.swift
+//  MovieApp-Grid-SwiftUI
+//
+//  Created by Mitya Kim on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp_Grid_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
